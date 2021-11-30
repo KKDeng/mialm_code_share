3eca3c741556e9c7d844209df808add129876e42
LAPTOP-4CMI79LS\huawei <dengkangkang@pku.edu.cn>
2021-11-30 09:06:50 +0800

    delete all

--- misc/setduplicat_pduplicat.m
+++
@@ -1,5 +0,0 @@
-function setduplicat_pduplicat(n)
- global  Dn pDn;
- Dn=sparse(DuplicationM(n));
- pDn=(Dn'*Dn)\Dn';
-end
--- runme_first.m
+++
@@ -1,6 +0,0 @@
-clc
-clear;
-close all;
-addpath misc
-addpath SSN_subproblem
-addpath (genpath('manopt'));
